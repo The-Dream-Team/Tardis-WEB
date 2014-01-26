185cc9c1ef3ae0eeb519e798a973edeaecc28e7f
Grant <Lerogero@gmail.com>
2014-01-26 20:05:36 +0000

    Works, let's test a new deploy script

--- deploy.php
+++ deploy.php
@@ -1,19 +1,9 @@
 <?php
-
-// Init vars
-$LOCAL_ROOT         = "/var/www";
-$LOCAL_REPO_NAME    = "Tardis-WEB";
-$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
-$REMOTE_REPO        = "git@github.com:The-Dream-Team/Tardis-WEB.git";
-$DESIRED_BRANCH     = "master";
-
-// Delete local repo if it exists
-if (file_exists($LOCAL_REPO)) {
-    shell_exec("rm -rf {$LOCAL_REPO}");
+ 
+// Use in the "Post-Receive URLs" section of your GitHub repo.
+ 
+if ( $_POST['payload'] ) {
+  shell_exec( 'cd /var/www/Tardis-WEB/ && git reset --hard HEAD && git pull' );
 }
-
-// Clone fresh repo from github using desired local repo name and checkout the desired branch
-echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$BRANCH}");
-
-die("done " . mktime());
-?>
+ 
+?
