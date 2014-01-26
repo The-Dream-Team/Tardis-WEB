05d45376e4ba33900db26f41aa3e792252333e07
Grant <Lerogero@gmail.com>
2014-01-26 18:06:05 +0000

    Placeholder

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-// To be completed
+<!DOCTYPE html>
+ 
+<html>
+ 
+<head>
+    <meta http-equiv="Content-type" content="text/html;charset=UTF-8">
+    <title>Site Maintenance</title>
+    <style type="text/css">
+      body { text-align: center; padding: 150px; }
+      h1 { font-size: 50px; }
+      body { font: 20px Helvetica, sans-serif; color: #333; }
+      #article { display: block; text-align: left; width: 650px; margin: 0 auto; }
+      a { color: #dc8100; text-decoration: none; }
+      a:hover { color: #333; text-decoration: none; }
+    </style>
+ 
+</head>
+<body>
+    <div id="article">
+    <h1>Under Construction</h1>
+    <div>
+        <p>Website is currently under construction and will be available soon</p>
+        <p>&mdash; The Dream Team</p>
+    </div>
+    </div>
+</body>
+</html>
