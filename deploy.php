5e401bf6a5c0a34c7af21f5ae83adb2c9e129e44
Grant <Lerogero@gmail.com>
2014-01-26 19:53:58 +0000

    Update deploy.php

--- deploy.php
+++ deploy.php
@@ -1,50 +1,19 @@
 <?php
-	/**
-	 * GIT DEPLOYMENT SCRIPT
-	 *
-	 * Used for automatically deploying websites via github or bitbucket, more deets here:
-	 *
-	 *		https://gist.github.com/1809044
-	 */
 
-	// The commands
-	$commands = array(
-		'echo $PWD',
-		'whoami',
-		'git pull',
-		'git status',
-		'git submodule sync',
-		'git submodule update',
-		'git submodule status',
-	);
+// Init vars
+$LOCAL_ROOT         = "/var/www";
+$LOCAL_REPO_NAME    = "Tardis-WEB";
+$LOCAL_REPO         = "{$LOCAL_ROOT}/{$LOCAL_REPO_NAME}";
+$REMOTE_REPO        = "git@github.com:The-Dream-Team/Tardis-WEB.git";
+$DESIRED_BRANCH     = "master";
 
-	// Run the commands for output
-	$output = '';
-	foreach($commands AS $command){
-		// Run it
-		$tmp = shell_exec($command);
-		// Output
-		$output .= "<span style=\"color: #6BE234;\">\$</span> <span style=\"color: #729FCF;\">{$command}\n</span>";
-		$output .= htmlentities(trim($tmp)) . "\n";
-	}
+// Delete local repo if it exists
+if (file_exists($LOCAL_REPO)) {
+    shell_exec("rm -rf {$LOCAL_REPO}");
+}
 
-	// Make it pretty for manual user access (and why not?)
+// Clone fresh repo from github using desired local repo name and checkout the desired branch
+echo shell_exec("cd {$LOCAL_ROOT} && git clone {$REMOTE_REPO} {$LOCAL_REPO_NAME} && cd {$LOCAL_REPO} && git checkout {$BRANCH}");
+
+die("done " . mktime());
 ?>
-<!DOCTYPE HTML>
-<html lang="en-US">
-<head>
-	<meta charset="UTF-8">
-	<title>GIT DEPLOYMENT SCRIPT</title>
-</head>
-<body style="background-color: #000000; color: #FFFFFF; font-weight: bold; padding: 0 10px;">
-<pre>
- .  ____  .    ____________________________
- |/      \|   |                            |
-[| <span style="color: #FF0000;">&hearts;    &hearts;</span> |]  | Git Deployment Script v0.1 |
- |___==___|  /              &copy; oodavid 2012 |
-              |____________________________|
-
-<?php echo $output; ?>
-</pre>
-</body>
-</html>
