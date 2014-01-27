883683f721b3b6119f90a79c49a9a02e37d8709a
katiewood <katie.wood3@hotmail.com>
2014-01-27 19:45:51 +0000

    change button name

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
       			<div class="form-group">
         			<input type="text" class="form-control" placeholder="Search">
      			</div>
-      			<button type="submit" class="btn btn-default">Submit</button>
+      			<button type="submit" class="btn btn-default">Search</button>
     		</form>
           </ul>
         </div><!--/.nav-collapse -->
