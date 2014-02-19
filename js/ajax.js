504bf419160f18e1d17cec1fff60cfcca59223d1
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2014-02-19 19:29:36 +0000

    Missing curly bracket!

--- js/ajax.js
+++ js/ajax.js
@@ -3,7 +3,7 @@
 function ajaxFunction(){
 
  	 $(document).ready(function() {
-	 $("#progressbar").css({"width":"100%");
+	 $("#progressbar").css({"width":"100%"});
      $("#loading").delay(1500).hide();
 	 $("#results").show();
    });
