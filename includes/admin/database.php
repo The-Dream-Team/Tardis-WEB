ecdab2dbf231fe3ce18c6061c8f78fdc80925bbe
Grant <Lerogero@gmail.com>
2014-02-19 15:46:28 +0000

    Fixed sql query

--- includes/admin/database.php
+++ includes/admin/database.php
@@ -9,16 +9,16 @@ echo "<table class='table table-striped'>
 				<tr>
 					<th>ID</th>
 					<th>Rank</th>
-					<th>Name</th>
+					<th>Player Name</th>
 					<th>Distance</th>
 				</tr>";
 
 while($row = mysqli_fetch_array($result))
 {
 	echo "<tr>";
-	echo "<td>" . $row['ID'] . "</td>";
+	echo "<td>" . $row['PlayerID'] . "</td>";
 	echo "<td>" . $rank++ . "</td>";
-	echo "<td>" . $row['Name'] . "</td>";
+	echo "<td>" . $row['PlayerName'] . "</td>";
 	echo "<td>" . $row['Distance'] . "</td>";
 	echo "<td><a href='edituser.php?id=". $row['ID'] . "'>EDIT</a></td>";
 	echo "<td><a href='delete.php?id=". $row['ID'] . "'>DELETE</a></td>";
