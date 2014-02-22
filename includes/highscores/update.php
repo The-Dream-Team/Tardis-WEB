159f2b52c19d7335396224d5dadc35a4b9e13e3e
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2014-02-22 00:45:34 +0000

    Change GET to POST

--- includes/highscores/update.php
+++ includes/highscores/update.php
@@ -1,7 +1,7 @@
 <?php
 
-$username = $_GET['username'];
-$distance = $_GET['distance'];
+$username = $_POST['username'];
+$distance = $_POST['distance'];
 $con = mysqli_connect("localhost","thedreamteam","El3ven","SSA") or die ("Error " . mysqli_error($con));
 
 $query = mysqli_query($con,"UPDATE `Top Player` SET Distance=$distance WHERE PlayerName='$username'");
