c3d80fa2725c489760934e3b0ce7dd0a5d13913a
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2014-02-22 18:13:40 +0000

    Username checks, add user if not existing.

--- includes/highscores/update.php
+++ includes/highscores/update.php
@@ -4,7 +4,16 @@ $username = $_GET['username'];
 $distance = $_GET['distance'];
 $con = mysqli_connect("localhost","thedreamteam","El3ven","SSA") or die ("Error " . mysqli_error($con));
 
-$query = mysqli_query($con,"UPDATE `Top Player` SET Distance=$distance WHERE PlayerName='$username'");
+// Let's check to see if the user exists
+
+$usercheck = mysqli_query($con, "SELECT * FROM `Top Player` WHERE PlayerName='$username'");
+if(mysql_num_rows($usercheck)>=1) {
+    $query = mysqli_query($con,"UPDATE `Top Player` SET Distance=$distance WHERE PlayerName='$username'");
+} else {
+    $query = mysqli_query($con,"INSERT INTO `Top Player` (`PlayerID`, `PlayerName`, `Distance`) VALUES (NULL, '$username', '$distance')");
+}
+
+
 echo  $_GET['username'];
 echo " ";
 echo  $_GET['distance'];
