bd608a07701d9ac200ecd2ea32ff2109968b58b0
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2014-02-23 00:43:49 +0000

    Only update score if beating previous score

--- includes/highscores/update.php
+++ includes/highscores/update.php
@@ -2,19 +2,25 @@
 
 $username = $_GET['username'];
 $distance = $_GET['distance'];
-$con = mysqli_connect("localhost","thedreamteam","El3ven","SSA") or die ("Error " . mysqli_error($con));
+$con = mysqli_connect("localhost", "thedreamteam", "El3ven", "SSA") or die ("Error " . mysqli_error($con));
 
 // Let's check to see if the user exists
 
 $usercheck = mysqli_query($con, "SELECT * FROM `Top Player` WHERE PlayerName='$username'");
-if(mysql_num_rows($usercheck)>=1) {
-    $query = mysqli_query($con,"UPDATE `Top Player` SET Distance=$distance WHERE PlayerName='$username'");
+$scorecheck = mysqli_query($con, "SELECT `Distance` FROM `Top Player` WHERE PlayerName='$username' AND Distance='$distance'");
+
+if (mysql_num_rows($usercheck) >= 1) {
+    if ($distance > $scorecheck) {
+        $query = mysqli_query($con, "UPDATE `Top Player` SET Distance=$distance WHERE PlayerName='$username'");
+    } else {
+        echo "Did not beat previous high score";
+    }
 } else {
-    $query = mysqli_query($con,"INSERT INTO `Top Player` (`PlayerID`, `PlayerName`, `Distance`) VALUES (NULL, '$username', '$distance')");
+    $query = mysqli_query($con, "INSERT INTO `Top Player` (`PlayerID`, `PlayerName`, `Distance`) VALUES (NULL, '$username', '$distance')");
 }
 
 
-echo  $_GET['username'];
+echo $_GET['username'];
 echo " ";
-echo  $_GET['distance'];
+echo $_GET['distance'];
 ?>
