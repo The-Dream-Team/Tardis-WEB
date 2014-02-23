64d2986cf9418acddee105e58a101d9a0b411764
Jordan Taylor <j.n.taylor@wlv.ac.uk>
2014-02-23 00:50:36 +0000

    Add additional check

--- includes/highscores/update.php
+++ includes/highscores/update.php
@@ -8,10 +8,11 @@ $con = mysqli_connect("localhost", "thedreamteam", "El3ven", "SSA") or die ("Err
 
 $usercheck = mysqli_query($con, "SELECT * FROM `Top Player` WHERE PlayerName='$username'");
 $scorecheck = mysqli_query($con, "SELECT `Distance` FROM `Top Player` WHERE PlayerName='$username' AND Distance='$distance'");
+$id = mysqli_query($con, "SELECT `PlayerID` FROM `Top Player` WHERE PlayerName='$username' AND Distance='$distance'");
 
 if (mysql_num_rows($usercheck) >= 1) {
     if ($distance > $scorecheck) {
-        $query = mysqli_query($con, "UPDATE `Top Player` SET Distance=$distance WHERE PlayerName='$username'");
+        $query = mysqli_query($con, "UPDATE `Top Player` SET Distance=$distance WHERE PlayerName='$username' AND PlayerID='$id'");
     } else {
         echo "Did not beat previous high score";
     }
