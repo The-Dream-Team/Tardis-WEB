2f88ad9bc21fa32d07ecf08ed2eb290b8342de12
Grant <Lerogero@gmail.com>
2014-02-24 16:07:59 +0000

    Added login page and updates

---
+++ adminlogin.php
@@ -0,0 +1,20 @@
+<?php
+$pageTitle = "adminlogin";
+$page = "adminlogin";
+include 'includes/head.php';
+?>
+  <body>
+    <div class="container">
+      <form class="form-signin" action="login.php" method="post">
+        <h2 class="form-signin-heading">Admin Panel</h2>
+        <input type="email" class="form-control" placeholder="Username" required autofocus>
+        <input type="password" class="form-control" placeholder="Password" required> 
+        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+      </form>
+
+    </div>
+  </body>
+</html>
+<?php 
+include 'includes/js.php';
+?>
