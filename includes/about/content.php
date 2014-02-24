cc3666bdb774c821b457344bd7fc4f8c305490dd
gurnam17 <gurnam_sangat@hotmail.com>
2014-02-24 10:49:17 +0000

    add info on the about page

--- includes/about/content.php
+++ includes/about/content.php
@@ -1,5 +1,12 @@
 <div class="container">
 
+<p> As our Collaborative Development project at university we have developed a java game. </p>
+
+<p> The objective of the game is to avoid the enemies which are falling and travel the further distance. In addition to this we have included power up which will help the user where necessary. </p>   
+
+
+
+
     <hr>
 
 </div>
