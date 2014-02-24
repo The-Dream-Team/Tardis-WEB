fc81f7a575bea17bb5e0c5aca376c708f0bff9b0
katiewood <katie.wood3@hotmail.com>
2014-02-24 12:00:14 +0000

    update text on about us

--- includes/about/content.php
+++ includes/about/content.php
@@ -1,6 +1,6 @@
 <div class="container">
  <div class="col-md-8">
-<p> As our Collaborative Development project at university we have developed a java game, called Super Spaceship Adventure!! </p>
+<p> As our Collaborative Development project at university we have developed a java game, called <b>Super Spaceship Adventure!!</b> </p>
 
 <p> The objective of the game is to avoid the enemies which are falling whilst travelling through space, trying to get the further distance away from earth. In addition to this we have included power ups which will help the user where necessary.   
 The purpose for this website is the main hub for the game, where game players are able to see the database, with there username, and find out where they are positioned on this. 
