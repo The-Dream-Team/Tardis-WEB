89a32681139fbc84c20fb585424618db5aa731fd
katiewood <katie.wood3@hotmail.com>
2014-02-24 16:03:36 +0000

    add info to about

--- includes/about/content.php
+++ includes/about/content.php
@@ -1,19 +1,48 @@
 <div class="container">
- <div class="col-md-8">
-<p> As our Collaborative Development project at university we have developed a java game, called <b>Super Spaceship Adventure!!</b> </p>
-
-<p> The objective of the game is to avoid the enemies which are falling whilst travelling through space, trying to get the further distance away from earth. In addition to this we have included power ups which will help the user where necessary.   
-<p>The purpose for this website is the main hub for the game, where game players are able to see the database, with there username, and find out where they are positioned on this. </p>
- 
-
-</p>
-</div>
+ 	<div class="col-md-8">
+		<p> As our Collaborative Development project at university we have developed a java game, called <b>Super Spaceship Adventure!!</b> </p>
+	    <p> The objective of the game is to avoid the enemies which are falling whilst travelling through space, trying to get the further distance away from earth. In addition to this we have included power ups which will help the user where necessary.   
+		<p>The purpose for this website is the main hub for the game, where game players are able to see the database, with there username, and find out where they are positioned on this. </p>
+ 		</p>
+	</div>
+	
+	<div class="col-md-4">
+		<img src="img/java_main.png"/>
+	</div>  
+	
+ <h1>About the Staff</h1>
+	<div class="col-md-4">
+			<h2>Katie Wood</h2>
+           <p>Facebook: www.facebook.com/katieLMAO </p>
+            <p>Twitter: www.twitter.com/katieee94xxx </p>
+    </div>
+    
+  
+    <div class="col-md-4">
+            <h2>Jordan Taylor</h2>
+           <p>Facebook: www.facebook.com/JTaylor69 </p>
+            <p>Twitter: www.twitter.com/JTaylor69 </p>
+    </div>
+    
+    <div class="col-md-4">
+            <h2>Ping Sam</h2>
+            <p>Facebook: www.facebook.com/ping.sam.73 </p>
+            <p>Twitter: N/A </p>
+          </div>
+    
 <div class="col-md-4">
-<img src="img/java_main.png"/>
-</div>  
-
-
-
+            <h2>Gurnam Sangat</h2>
+            <p>Facebook: www.facebook.com/gurnam.sangat </p>
+            <p>Twitter: www.twitter.com/Gurnam17 </p>
+            
+    </div>
+    
+    <div class="col-md-4">
+            <h2>Grant Neal</h2>
+            <p>Facebook: N/A</p>
+            <p>Twitter: N/A</p>
+    </div>
+    
     <hr>
 
 </div>
