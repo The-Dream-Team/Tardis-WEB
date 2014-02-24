ed97d0952b9f5d7b6beb1d54084dab88fdcf5a8a
katiewood <katie.wood3@hotmail.com>
2014-02-24 11:53:23 +0000

    correct text

--- includes/about/content.php
+++ includes/about/content.php
@@ -2,8 +2,8 @@
  <div class="col-md-8">
 <p> As our Collaborative Development project at university we have developed a java game. </p>
 
-<p> The objective of the game is to avoid the enemies which are falling and travel the further distance. In addition to this we have included power up which will help the user where necessary.
-The purpose for this website is the main hub for the game.
+<p> The objective of the game is to avoid the enemies which are falling whilst travelling through space, trying to get the further distance away from earth. In addition to this we have included power ups which will help the user where necessary.   
+The purpose for this website is the main hub for the game, where game players are able to see the database, with there username, and find out where they are positioned on this. 
  
 
 </p>
