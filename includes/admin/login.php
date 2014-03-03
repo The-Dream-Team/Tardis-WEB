cb8b53bc1079a406b838434dae75b07e03904ff4
Grant <Lerogero@gmail.com>
2014-03-03 15:47:01 +0000

    Copied code

--- includes/admin/login.php
+++ includes/admin/login.php
@@ -1,30 +1,25 @@
-<?php
-session_start();
+<?php session_start();
+
 include "includes/sqllogin.php";
+	
+		
+	$result = mysqli_query($con,"SELECT ID, Username, Password FROM admin WHERE Username='$_POST[Username]'");
 
+	$row = mysqli_fetch_row($result);
+	$id = $row[0];
+	$username = $row[1];
+	$password = $row[2];
+	if ($_POST['Password'] != $password)
+	{
+		echo "Bad Username or Password";	
+	} 
+		else {
+			// Create Sessions and cookies
+			$_SESSION['userid'] = $id;
+			$_SESSION['username'] = $username;
+			$_SESSION['password'] = $password;
+			header("Location: ../../admin.php");
+			exit;
+			}
 
-$result = mysqli_query($con,"SELECT ID, Username, Password FROM admins WHERE Username='$_POST[Username]'");
-
-$row = mysqli_fetch_row($result);
-$id = $row[0];
-$username = $row[1];
-$password = $row[2];
-
-echo $id;
-echo $username;
-echo $password;
-
-
-if ($_POST['Password'] != $password)
-{
-	echo "Bad Password";
-}
-else {
-	// Create Sessions
-	$_SESSION['userid'] = $id;
-	$_SESSION['username'] = $username;
-	$_SESSION['password'] = $password;
-	header("Location: ../../admin.php");
-	exit;
-}
 ?>
