e128ca476b527d369e3d6ea256733b49b6f7621b
Grant <Lerogero@gmail.com>
2014-03-03 14:34:19 +0000

    Updated session check

--- admin.php
+++ admin.php
@@ -9,14 +9,17 @@ include 'includes/head.php';
 <?php
 session_start();
 
-if (isset($_SESSION['userid']) && $_SESSION['username'] && $_SESSION['password'] == '') {
+if (!isset($_SESSION['userid']) && $_SESSION['username'] && $_SESSION['password']) {
 header ("Location: adminlogin.php");
+exit();
 }
+else {
     include 'includes/nav.php';
     include 'includes/admin/jumbotron.php';
     include 'includes/admin/database.php';
     include 'includes/admin/adminfooter.php';
     include 'includes/js.php';
+}
 ?>
 </body>
 </html>
