59fb26375cffd2cb83d93dcc7d0038d93e32b167
Grant <Lerogero@gmail.com>
2014-03-03 17:35:33 +0000

    Added name to form

--- adminlogin.php
+++ adminlogin.php
@@ -7,8 +7,8 @@ include 'includes/head.php';
     <div class="container">
       <form class="form-signin" method="post" action="includes/admin/login.php">
         <h2 class="form-signin-heading">Admin Panel</h2>
-        <input type="Username" class="form-control" placeholder="Username" required autofocus>
-        <input type="Password" class="form-control" placeholder="Password" required> 
+        <input type="text" name="Username" class="form-control" placeholder="Username" required autofocus>
+        <input type="text" name="Password" class="form-control" placeholder="Password" required> 
         <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
       </form>
 
