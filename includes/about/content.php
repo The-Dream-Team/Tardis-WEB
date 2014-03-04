1ebc5b78b4b8b7d09e5c60ed851ce55d05bb0f70
katiewood <katie.wood3@hotmail.com>
2014-03-04 15:24:06 +0000

    update text

--- includes/about/content.php
+++ includes/about/content.php
@@ -4,8 +4,7 @@
  	
 		<p> We are a group of students in our second year at university studying a Computing course, for our Collaborative Development project we are currently developing a game  called <strong>Super Spaceship Adventure!</strong> using java as the programming language. </p>
 	    <p> The objective of the game is to avoid the enemies which are falling whilst travelling through space, trying to get the further distance away from earth. In addition to this we have included power ups which will help the user where necessary.</p>   
-		<p>The purpose for this website is the main hub for the game, where game players are able to see the highscores database, with their username, and find out where they are positioned on the rank. </p>
- 		
+		<p>The purpose for this website is the main hub for the game, where game players are able to access the highscores database, allowing them to view their highscore, which will be displayed next to their username and find out where they are positioned on the rank.</p>
 	</div>
 	
 	<div class="col-md-4">
