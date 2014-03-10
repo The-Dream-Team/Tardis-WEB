43986fbba4bb8045da9629fd1c0feba0b4a1d63a
Grant <Lerogero@gmail.com>
2014-03-10 13:32:39 +0000

    Updated delete query

--- delete.php
+++ delete.php
@@ -2,11 +2,10 @@
 
 include "../sqllogin.php";
 
-mysqli_query($con,"DELETE FROM `Top Player` WHERE PlayerID='$_GET[id]'");
+mysqli_query($con,"DELETE FROM Top Player WHERE PlayerID='$_GET[id]'");
 
 mysqli_close($con);
 
 header("Location: admin.php");
 exit;
 
-?>
