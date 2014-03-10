14f90579a908d0786277622169ea05cad27dc284
Grant <Lerogero@gmail.com>
2014-03-10 13:01:39 +0000

    Corrected SQL error

--- delete.php
+++ delete.php
@@ -2,7 +2,7 @@
 
 include "../sqllogin.php";
 
-mysqli_query($con,"DELETE FROM Top Players WHERE ID='$_GET[id]'");
+mysqli_query($con,"DELETE FROM Top Players WHERE ID='$_GET[PlayerID]'");
 
 mysqli_close($con);
 
