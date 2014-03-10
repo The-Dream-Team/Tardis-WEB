7bba5c7dd269985ec6e18f4ed70871367a79f6d8
Grant <Lerogero@gmail.com>
2014-03-10 14:51:06 +0000

    Updated delete query

--- includes/admin/delete.php
+++ includes/admin/delete.php
@@ -4,12 +4,10 @@ include 'includes/sqllogin.php';
 
 $ID = $_GET['id'];
 
-echo $ID;
-
-mysqli_query($con,"DELETE FROM `Top Player` WHERE `PlayerID` ='$ID' LIMIT 1");
+mysqli_query($con,"DELETE FROM `Top Player` WHERE PlayerID='$ID' LIMIT 1");
 
 mysqli_close($con);
 
-//header("Location: ../../admin.php");
+header("Location: ../../admin.php");
 exit;
 
