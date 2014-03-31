c0408e9ccb40738f6aa60b0ecfecb4d012ad6a60
Grant <Lerogero@gmail.com>
2014-03-31 13:02:00 +0100

    Added download to latest version

--- includes/home/carousel.php
+++ includes/home/carousel.php
@@ -24,7 +24,7 @@
                 <div class="carousel-caption" style="margin-bottom: 50px;">
                     <img class="carousel-imgresponsive" src="img/textdownload.png" alt="Download Game text">
                     <p></p>
-                    <p><a class="btn btn-lg btn-success" href="http://37.187.75.63:8080/job/SSA/lastSuccessfulBuild/artifact/target/SSA.jar" role="button"><span
+                    <p><a class="btn btn-lg btn-success" href="/includes/Downloads/Super_Spaceship_Adventure-0.10.2-BETA.jar" role="button"><span
                                 class="glyphicon glyphicon-download-alt"></span></a></p>
                 </div>
             </div>
